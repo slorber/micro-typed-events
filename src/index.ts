ad8de937d1629af73467d1962fbbb11f8acc2ca9
slorber <lorber.sebastien@gmail.com>
2019-03-08 11:10:04 +0100

    take care of user trying to unsub inside a listener

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,31 @@ interface TypedEvents<Listener extends (...args: any[]) => void> {
   emit: (...args: ArgumentsType<Listener>) => void;
 }
 
-
 // This overload has same signature as overloaded function, and ensures the initial fn signature will be choosen
 // in priority instead of createEvents<T0> overload
 export function createEvents<
   Listener extends (...args: any[]) => void
-  >(): TypedEvents<Listener>
+>(): TypedEvents<Listener>;
 
 // Add some convenient type overloads
 // can this be generalized and avoided?
 export function createEvents<T0>(): TypedEvents<(arg0: T0) => void>;
 export function createEvents<T0, T1>(): TypedEvents<
   (arg0: T0, arg1: T1) => void
-  >;
+>;
 export function createEvents<T0, T1, T2>(): TypedEvents<
   (arg0: T0, arg1: T1, arg2: T2) => void
-  >;
+>;
 export function createEvents<T0, T1, T2, T3>(): TypedEvents<
   (arg0: T0, arg1: T1, arg2: T2, arg3: T3) => void
-  >;
+>;
 export function createEvents<T0, T1, T2, T3, T4>(): TypedEvents<
   (arg0: T0, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => void
-  >;
+>;
 
 export function createEvents<T0, T1, T2, T3, T4, T5>(): TypedEvents<
   (arg0: T0, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => void
-  >;
+>;
 
 export function createEvents<
   Listener extends (...args: any[]) => void
@@ -49,7 +48,12 @@ export function createEvents<
       };
     },
     emit: (...args: ArgumentsType<Listener>) => {
-      listeners.forEach(listener => listener(...args));
+      // take care of user trying to unsub inside a listener
+      [...listeners].forEach(listener => {
+        if (listeners.indexOf(listener) !== -1) {
+          listener(...args);
+        }
+      });
     },
   };
 }
--- src/test.ts
+++ src/test.ts
@@ -24,6 +24,74 @@ test('basic usecase', () => {
   expect(listener2.mock.calls[1]).toEqual(['b']);
 });
 
+
+test('unsub 2 times is no-op', () => {
+  const events = createEvents<string>();
+  const listener1 = jest.fn();
+  const unsub1 = events.subscribe(listener1);
+
+  events.emit('a');
+  unsub1();
+  unsub1();
+  events.emit('b');
+
+  expect(listener1).toHaveBeenCalledTimes(1);
+  expect(listener1.mock.calls[0]).toEqual(['a']);
+});
+
+test('unsub inside listener does not mess things up', () => {
+  const events = createEvents<string>();
+  const listener1 = jest.fn();
+  const listener2 = jest.fn();
+  const listener3 = jest.fn();
+
+  let unsub1: any;
+  let unsub2: any;
+  let unsub3: any;
+  let unsubSelf: any;
+
+  let unsubCallCount = 0;
+  const unsubListener = jest.fn(() => {
+    unsubCallCount++;
+    if (unsubCallCount === 1) {
+      unsub1();
+      unsub3();
+    } else if (unsubCallCount === 2) {
+      unsubSelf();
+    } else if (unsubCallCount === 3) {
+      unsub2(); // Should never happen because unsubListener has been unsubscribed
+    }
+  });
+
+  unsub1 = events.subscribe(listener1);
+  unsubSelf = events.subscribe(unsubListener);
+  unsub2 = events.subscribe(listener2);
+  unsub3 = events.subscribe(listener3);
+
+  events.emit('a');
+  events.emit('b');
+  events.emit('c');
+  events.emit('d');
+  events.emit('e');
+
+  expect(unsubListener).toHaveBeenCalledTimes(2);
+  expect(unsubListener.mock.calls[0]).toEqual(['a']);
+  expect(unsubListener.mock.calls[1]).toEqual(['b']);
+
+  expect(listener1).toHaveBeenCalledTimes(1);
+  expect(listener1.mock.calls[0]).toEqual(['a']);
+
+  expect(listener2).toHaveBeenCalledTimes(5);
+  expect(listener2.mock.calls[0]).toEqual(['a']);
+  expect(listener2.mock.calls[1]).toEqual(['b']);
+  expect(listener2.mock.calls[2]).toEqual(['c']);
+  expect(listener2.mock.calls[3]).toEqual(['d']);
+  expect(listener2.mock.calls[4]).toEqual(['e']);
+
+  // Should not have been called because unsub ran just before
+  expect(listener3).toHaveBeenCalledTimes(0);
+});
+
 test('multiple args usecase', () => {
   const events = createEvents<string, number>();
   const listener = jest.fn();
@@ -41,10 +109,20 @@ test('object arg usecase', () => {
 });
 
 test('some complex listener type usecase', () => {
-  type ListenerType = (str: string, num: number, ...args: any[]) => void
+  type ListenerType = (str: string, num: number, ...args: any[]) => void;
   const events = createEvents<ListenerType>();
   const listener = jest.fn();
   events.subscribe(listener);
   events.emit('hey', 1, 'blaa', 'whatever', 'you', 'want', 1, 2, 3);
-  expect(listener).toHaveBeenCalledWith('hey', 1, 'blaa', 'whatever', 'you', 'want', 1, 2, 3);
+  expect(listener).toHaveBeenCalledWith(
+    'hey',
+    1,
+    'blaa',
+    'whatever',
+    'you',
+    'want',
+    1,
+    2,
+    3,
+  );
 });
